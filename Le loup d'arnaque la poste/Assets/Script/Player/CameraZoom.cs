175cf81dc7126436600cfef09634df88de0823bf
Unknown <guillaume.vanson@outlook.fr>
2019-05-18 10:22:28 +0200

    Deplacement basic

---
+++ Le loup d'arnaque la poste/Assets/Script/Player/CameraZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public float minSize = 2;
+    public float maxSize = 10;
+    public float zoomSpeed = 1f;
+
+    private Camera cam;
+
+    private void OnEnable()
+    {
+        cam = this.GetComponent<Camera>();
+        if (cam == null)
+            cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float zoom = Input.GetAxis("Mouse ScrollWheel");
+        cam.orthographicSize += zoom * zoomSpeed;
+
+        if (cam.orthographicSize < minSize)
+            cam.orthographicSize = minSize;
+        else if(cam.orthographicSize > maxSize)
+            cam.orthographicSize = maxSize;
+    }
+}
---
+++ Le loup d'arnaque la poste/Assets/Script/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    private Vector2 movement;
+    private Transform camTransform;
+
+    private void Start()
+    {
+        camTransform = Camera.main.transform;
+        camTransform.position = new Vector3(transform.position.x, transform.position.y, camTransform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3( speed * inputX,  speed * inputY,0) * Time.deltaTime;
+        camTransform.position = new Vector3(transform.position.x, transform.position.y, camTransform.position.z);
+    }
+}
