bc27b16bbc5adf1e77e3f35d63be4bb72e860f96
Unknown <bluchier@laposte.net>
2019-05-18 13:51:47 +0200

    Inventory

--- Le loup d'arnaque la poste/Assets/Script/Player/Inventory.cs
+++ Le loup d'arnaque la poste/Assets/Script/Player/Inventory.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Item : ScriptableObject
+public class Inventory
 {
-    public string itemName;
+    private List<Item> items = new List<Item>();
 }
