191e9ce6a844b4e2ef590106bea38fd5c5cd163d
Unknown <guillaume.vanson@outlook.fr>
2019-05-19 14:40:23 +0200

    big hope

---
+++ Le loup d'arnaque la poste/Assets/Script/GameObject/ItemPrefab.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPrefab : MonoBehaviour
+{
+    public Pickable item;
+    public SpriteRenderer childRend;
+
+    public void Init(Vector3 position, Pickable item)
+    {
+        transform.position = position;
+        this.item = item;
+        transform.GetComponent<SpriteRenderer>().sprite = item.icon;
+    }
+
+    public void CanBeGather(bool boolean)
+    {
+        Debug.Log(boolean);
+        childRend.enabled = boolean;
+    }
+}
