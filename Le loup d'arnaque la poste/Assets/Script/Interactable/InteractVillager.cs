483da183453d42beffaad77e0ded65b48aae16fd
Unknown <guillaume.vanson@outlook.fr>
2019-05-19 18:13:23 +0200

    test

--- Le loup d'arnaque la poste/Assets/Script/Interactable/InteractVillager.cs
+++ Le loup d'arnaque la poste/Assets/Script/Interactable/InteractVillager.cs
@@ -6,12 +6,24 @@ using UnityEngine.UI;
 public class InteractVillager : MonoBehaviour, Interactable
 {
     public SpriteRenderer childRend;
+<<<<<<< Updated upstream
     public SpriteRenderer textrender;
+=======
+    public Text textContainer;
+    public GameObject textrender;
+>>>>>>> Stashed changes
     public string text;
     public float displayTime;
     private bool isShow;
 
+<<<<<<< Updated upstream
 
+=======
+    public void Start()
+    {
+        textContainer.text = text;
+    }
+>>>>>>> Stashed changes
 
     public void CanBeInteract(bool boolean)
     {
@@ -33,10 +45,18 @@ public class InteractVillager : MonoBehaviour, Interactable
     public IEnumerator displayText()
     {
 
+<<<<<<< Updated upstream
         textrender.enabled = true;
         isShow = true;
          yield return new WaitForSeconds(displayTime);
         isShow = false;
         textrender.enabled = false;
+=======
+        textrender.SetActive(true);
+        isShow = true;
+         yield return new WaitForSeconds(displayTime);
+        isShow = false;
+        textrender.SetActive(false);
+>>>>>>> Stashed changes
     }
 }
